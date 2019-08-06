e7d2b7cc006157d91e09dd727179d3cc84484b5e
Joshmatjjen <joshmatparrot@gmail.com>
2019-08-06 23:54:34 +0100

    new

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Trexology\ReviewRateable\ReviewRateableServiceProvider::class,
-        Laravel\Passport\PassportServiceProvider::class
+        // Laravel\Passport\PassportServiceProvider::class
 
     ],
 
