7b92834dbc1f2876e719c8bf6b50ee14da409773
Joshmatjjen <joshmatparrot@gmail.com>
2019-08-08 01:21:36 +0100

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Carsall88
                 </div>
 
                 <div class="links">
