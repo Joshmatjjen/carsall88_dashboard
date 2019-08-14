4f6efc25bf26791900dc06d5867055ecb7dc3683
Joshmatjjen <joshmatparrot@gmail.com>
2019-08-14 13:57:37 +0100

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(SubCategoriesTableSeeder::class);
+        $this->call(ServicesTableSeeder::class);
     }
 }
