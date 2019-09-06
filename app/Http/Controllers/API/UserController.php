477ab8b7afd6461eeb6c1b6fe7170e7aa4b1e482
Joshmatjjen <joshmatparrot@gmail.com>
2019-09-06 01:15:59 +0100

    new

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -117,10 +117,15 @@ class UserController extends Controller
         if($request->photo  != $currentPhoto){
 
             // Code to take only the extension of the files
-            // $name = time().'.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
-            $name = "profile-".time().".png";
+            $name = time().'.' . explode('/', explode(':', substr($request->photo, 0, strpos($request->photo, ';')))[1])[1];
 
-            \Image::make($request->photo)->encode('data-url')->save(public_path('img/profile/').$name);
+            \Image::make($request->photo)->save(public_path('img/profile/').$name);
+
+
+            // $name = "profile-".time().".png";
+
+            // \Image::make($request->photo)->encode('data-url')->save(public_path('img/profile/').$name);
+
 
             $request->merge(['photo' => $name]);
 
