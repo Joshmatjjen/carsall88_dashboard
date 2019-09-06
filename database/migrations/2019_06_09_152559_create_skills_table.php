006f24cbffd1a8517154a16f3e1c77088ba53194
Joshmatjjen <joshmatparrot@gmail.com>
2019-09-06 17:14:13 +0100

    new Skills

--- database/migrations/2019_06_09_152559_create_skills_table.php
+++ database/migrations/2019_06_09_152559_create_skills_table.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreateSkillsTable extends Migration
 {
+    use SoftDeletes;
     /**
      * Run the migrations.
      *
@@ -15,6 +17,14 @@ class CreateSkillsTable extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete( 'cascade' )
+            ->onUpdate( 'cascade' );
+            $table->bigInteger('service_id')->unsigned();
+            $table->foreign('service_id')->references('id')->on('services')
+            ->onDelete( 'cascade' )
+            ->onUpdate( 'cascade' );
             $table->timestamps();
         });
     }
